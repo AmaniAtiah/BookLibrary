61c8caabbfd2cd4c29114a284ee577e961736129
Amani_Atiah <amaniatiah@gmail.com>
2020-11-19 03:40:34 +0300

    add line 3

--- app/src/main/java/com/barmej/booklibrary1/MainActivity.java
+++ app/src/main/java/com/barmej/booklibrary1/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int number1 = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
